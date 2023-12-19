494f13219728e40137ceadb9664b5a46c79b173c
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-12-19 23:03:14 +0800

    first commit

---
+++ python/test.py
@@ -0,0 +1,18 @@
+import os
+from openai import OpenAI
+
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY environment variable is not set.")
+
+client = OpenAI(api_key=api_key)
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
+)
+
+print(completion.choices[0].message)
